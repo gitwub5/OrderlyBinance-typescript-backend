52b87e50dfd132593f8642b74c33b0763c21e853
GeonWoo SHIN <tonyw2@khu.ac.kr>
2024-07-15 18:04:18 +0900

    feat: restart function

--- src/index.ts
+++ src/index.ts
@@ -25,25 +25,37 @@ async function manageMultipleTokens(tokens: token[]) {
     }));
 }
 
-// TODO: 바이낸스랑 오덜리 함수들 클래스화
+async function shutdown() {
+    setForceStop(true);
+    setShouldStop(true);
+    console.log('Shutting down arbitrage...');
+    try {
+        await Promise.all(selectedTokens.map(token => closeAllPositions(token)));
+        await Promise.all(selectedTokens.map(token => cancelAllOrders(token)));
+        selectedTokens.forEach(token => disconnectClients(token));
+    } catch (err) {
+        console.error('Error during closing positions:', err);
+    } finally {
+        console.log('Exiting manageOrders...');
+        process.exit(0);
+    }
+}
+
+// Set up a restart every  23 hours and 55 minutes
+function setupDailyRestart() {
+    setTimeout(() => {
+        console.log('24 hours have passed. Restarting manageOrders...');
+        shutdown();
+    }, 23 * 60 * 60 * 1000 + 55 * 60 * 1000); //  23 hours and 55 minutes
+}
+
 async function init() {
     await createTables();
 
-    process.on('SIGINT', async () => {
-        setForceStop(true);
-        setShouldStop(true);
-        console.log('Received SIGINT. Stopping manageOrders...');
-        try {
-            await Promise.all(selectedTokens.map(token => closeAllPositions(token)));
-            await Promise.all(selectedTokens.map(token => cancelAllOrders(token)));
-            selectedTokens.forEach(token => disconnectClients(token));
-        } catch (err) {
-            console.error('Error during closing positions:', err);
-        } finally {
-            console.log('Exiting manageOrders...');
-            process.exit(0);
-        }
-    });
+    process.on('SIGINT', shutdown);
+
+    // Set up the daily restart timer
+    setupDailyRestart();
 
     await manageMultipleTokens(selectedTokens);
 }
